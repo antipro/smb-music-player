35d5043b4eca8ac24dc72d96aa63708c9bdcd32d
antipro <antipro@163.com>
2018-04-19 10:45:58 +0800

    update

--- core/src/modules/About.vue
+++ core/src/modules/About.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h3>Author</h3>
+    <p>antiproster</p>
+    <h3>Email</h3>
+    <p>antiproster@gmail.com</p>
+    <h3>Instruction</h3>
+    <p>If you have any question, please let me know. According to Google's policy, you can refund in 24 hours after purchase.</p>
+    <p>Regards</p>
   </div>
 </template>
+
+<style>
+.about { padding: 15px; }
+</style>
 
 <script>
 export default {
