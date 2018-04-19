123db339d50c74d6bf08f4ed95c6b570da4f38cb
antipro <antipro@163.com>
2018-04-19 10:53:53 +0800

    update

--- core/src/modules/About.vue
+++ core/src/modules/About.vue
@@ -5,7 +5,11 @@
     <h3>Email</h3>
     <p>antiproster@gmail.com</p>
     <h3>Instruction</h3>
-    <p>If you have any question, please let me know. According to Google's policy, you can refund in 24 hours after purchase.</p>
+    <p>This is a simple music player, It is used to listen music from nas/computer in your home/office.
+      It will index all of your music files, so they can be searched instantly.
+      Only file name can be indexed for now, but I will try to index tag info later.
+      If you have any question, please let me know.
+      According to Google's policy, you can refund in 24 hours after purchase.</p>
     <p>Regards</p>
   </div>
 </template>
